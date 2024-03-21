dbd18227f7fb9eb9c99ce462c98a7be8528826b7
Seamul <islamseamul@gmail.com>
2024-03-21 23:51:10 +0600

    get average temperature

--- src/home/serializer.py
+++ src/home/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import District,ForecastData
+from .models import District, ForecastData, ForecastMetaData
 
 
 class DistrictSerializer(serializers.ModelSerializer):
@@ -8,8 +8,16 @@ class DistrictSerializer(serializers.ModelSerializer):
         model = District
         fields = '__all__'
 
+
 class ForecastDataSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ForecastData
+        fields = '__all__'
+
+
+class ForecastMetaDataSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ForecastMetaData
         fields = '__all__'
--- src/home/views/average_temperature.py
+++ src/home/views/average_temperature.py
@@ -1,9 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from ..models import ForecastData
-
+from ..models import ForecastMetaData
+from ..serializer import ForecastMetaDataSerializer
 
 
 class GetLowestAverageTemperatures(APIView):
     def get(self, request):
-        return Response({"lowest_10_average_temperatures": "lowest_10_average_temperatures"})
+
+        lowest_temperatures = ForecastMetaData.objects.exclude(
+            average_temperature=None).order_by('average_temperature')[:10]
+
+        serializer = ForecastMetaDataSerializer(lowest_temperatures, many=True)
+
+        return Response({"lowest_10_average_temperatures": serializer.data})
