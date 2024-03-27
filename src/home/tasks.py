4d41bb4b113554308edce1738d03c4d286bfc931
Seamul_islam <sislam.idare.io>
2024-03-27 08:10:08 +0600

    Implement transactional logic to ensure compliance with ASID property

--- src/home/tasks.py
+++ src/home/tasks.py
@@ -4,16 +4,17 @@ from celery import shared_task
 
 from home.utils.forecast_update_helper import ForecastUpdateCommand
 from home.utils.districts_data_helper import get_districts_data
-
+from django.db import transaction
 
 @shared_task(bind=True)
 def run_forecast_update(self):
     """
     Orchestrates the workflow to update forecast data.
     """
-    print("==============Initiated forecast data update process===================")
-    districts_data = get_districts_data()
-    forecast_update_command = ForecastUpdateCommand(districts_data)
-    forecast_update_command.execute()
-    print("=================Forecast data update process completed=================")
-    return "Forecast data updated successfully"
+    with transaction.atomic():
+        print("==============Initiated forecast data update process===================")
+        districts_data = get_districts_data()
+        forecast_update_command = ForecastUpdateCommand(districts_data)
+        forecast_update_command.execute()
+        print("=================Forecast data update process completed=================")
+        return "Forecast data updated successfully"
