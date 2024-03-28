8c2611d14a180675d35647435a482322b60d0c7b
Seamul_islam <sislam.idare.io>
2024-03-28 10:31:01 +0600

    refactor district data helper

--- src/home/tasks.py
+++ src/home/tasks.py
@@ -3,7 +3,7 @@
 from celery import shared_task
 
 from home.utils.forecast_update_helper import ForecastUpdateCommand
-from home.utils.districts_data_helper import get_districts_data
+from home.utils.districts_data_helper import DistrictDataRetriever
 from django.db import transaction
 
 @shared_task(bind=True)
@@ -13,7 +13,8 @@ def run_forecast_update(self):
     """
     with transaction.atomic():
         print("==============Initiated forecast data update process===================")
-        districts_data = get_districts_data()
+        district_retriever = DistrictDataRetriever()
+        districts_data = district_retriever.get_districts_data()
         forecast_update_command = ForecastUpdateCommand(districts_data)
         forecast_update_command.execute()
         print("=================Forecast data update process completed=================")
--- src/home/utils/districts_data_helper.py
+++ src/home/utils/districts_data_helper.py
@@ -1,13 +1,31 @@
 from home.models import District
 from home.serializer import DistrictSerializer
 
-
-def get_districts_data():
+class DistrictDataRetriever:
     """
-    Retrieves districts data from the database.
-
-    Returns:
-    - list: List of district data.
+    Singleton class for retrieving districts data from the database.
     """
-    districts = District.objects.all()
-    return DistrictSerializer(districts, many=True).data
+
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        if not hasattr(self, '_initialized'):
+            self._initialized = True
+            # Initialization code here
+
+    def get_districts_data(self):
+        """
+        Retrieves districts data from the database.
+
+        Returns:
+        - list: List of district data.
+        """
+        districts = District.objects.all()
+        return DistrictSerializer(districts, many=True).data
+
+
